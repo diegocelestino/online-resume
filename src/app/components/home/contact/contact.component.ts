94be166334a2b4e47ccb0e4717757e6f7c05f901
Diego <diegocdsantos@gmail.com>
2023-09-03 15:00:24 -0300

    all the components had been implemented; now i gonna refactor all the css files

--- src/app/components/home/contact/contact.component.ts
+++ src/app/components/home/contact/contact.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {HomeService} from "../home.service";
+import {Animations} from "../animations";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss', '../home.component.scss'],
+  animations: [
+    Animations.openClose,
+  ],
 })
 export class ContactComponent {
+  state: boolean = false;
 
+  constructor(private homeService: HomeService) {
+  }
+
+
+  toggle() {
+    let img = document.getElementById("contact-dropdown");
+    this.state = this.homeService.toggle(img as HTMLElement, this.state);
+  }
 }

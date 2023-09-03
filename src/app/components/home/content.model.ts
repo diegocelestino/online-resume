0a48bbe48548651e48a07e9cdd1711ec16de7326
Diego <diegocdsantos@gmail.com>
2023-09-03 03:37:39 -0300

    preparing to isolate the carroussel component

---
+++ src/app/components/home/content.model.ts
@@ -0,0 +1,19 @@
+export class Content {
+  title: string;
+  subtitle: string;
+  text: string;
+  imgSource: string;
+
+  constructor(
+    title: string,
+    subtitle: string,
+    text: string,
+    imgSource: string
+  ) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.text = text;
+    this.imgSource = imgSource;
+  }
+}
+
--- src/app/components/home/experience/experience.component.ts
+++ src/app/components/home/experience/experience.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
 import {HomeService} from "../home.service";
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {animate, sequence, state, style, transition, trigger} from "@angular/animations";
+import {Content} from "../content.model";
 
 @Component({
   selector: 'app-experience',
@@ -24,15 +25,113 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
         animate('0.5s')
       ]),
     ]),
+    trigger('changeContent', [
+      state('in', style({
+        opacity: 1,
+        zIndex: 1,
+      })),
+      state('out', style({
+        zIndex: 1,
+        opacity: 1,
+      })),
+      transition('in <=> out', [
+        sequence([
+          style({
+            opacity: -1,
+            zIndex: 0,
+          }),
+          animate('0.5s'),
+        ]),
+      ]),
+    ]),
   ],
+
 })
 export class ExperienceComponent {
   state: boolean = false;
-  constructor(private homeService: HomeService){}
+  content: Content[] = [];
+  actualContent: number = 0;
+  changeView: boolean = true;
+  side: string = "";
 
-  toggle(){
+
+  toggle() {
     let img = document.getElementById("experience-dropdown");
     this.state = this.homeService.toggle(img as HTMLElement, this.state);
   }
+
+  constructor(private homeService: HomeService) {
+    this.content.push(this.jnj);
+    this.content.push(this.codelab);
+    this.content.push(this.saHall);
+  }
+
+  leftControl() {
+    this.changeView = !this.changeView;
+    this.side = "left";
+  }
+
+  rightControl() {
+    this.changeView = !this.changeView;
+    this.side = "right";
+  }
+
+  onAnimationEvent(event: any) {
+    if (this.side == "right") {
+      setTimeout(() => {
+        if (this.actualContent == this.content.length - 1) {
+          this.actualContent = 0;
+        } else {
+          this.actualContent += 1;
+        }
+      }, 200)
+    }
+
+    if (this.side == "left") {
+      setTimeout(() => {
+        if (this.actualContent == 0) {
+          this.actualContent = this.content.length - 1;
+        } else {
+          this.actualContent -= 1;
+        }
+      }, 200)
+    }
+  }
+
+  jnj = new Content(
+    "Johnson & Johnson Medtech",
+    "Experience Design Intern | 23/02 - nowadays,",
+    "Practice of design thinking methodologies to build new solutions focused on customer needs; support in" +
+    "building a toolbox of user-centered methodologies; development of guides and repositories for creating" +
+    "visual interfaces; creating and facilitating communities of practice; support for product squads applying" +
+    "UX and UI methods; development of actions to generate awareness of human-centered design.",
+    "../assets/img/jnj.png"
+  );
+
+  codelab = new Content(
+    "IFSP Codelab",
+    "Software Developer Intern | 22/05 - 22/12",
+    "Analysis and development of legacy project, survey and preparation of user stories" +
+    "and acceptance criteria; development of low and high fidelity prototypes, development of SPA's" +
+    "and REST API's with Spring and Angular; researching and developing solutions to implementation " +
+    "problems; group work, pair programming and sharing solutions; code versioning with Git/Github; " +
+    "presentations to the general public of the results obtained.",
+    "../assets/img/codelab.png"
+  );
+
+  saHall = new Content(
+    "Santo André City Hall",
+    "Public Employee - Child Development Officer | 19/06 - 22/12",
+    "Receive and refer children to their parents, by means of" +
+    "in order to help them reduce their anxieties and fears." +
+    "and fears; assisting in the acquisition and encouragement of " +
+    "walking and motor skills, by respecting the stages of development, " +
+    "encouraging them, with the teacher's supervision, to advance to the next " +
+    "stage; assisting the teacher in expanding the speech repertoire by naming objects, " +
+    "figures and musicality, so that they can communicate more effectively." +
+    "communicate more effectively;",
+    "../assets/img/creche.png"
+  );
+
 
 }
